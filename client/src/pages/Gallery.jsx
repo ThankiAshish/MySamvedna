b7798d5609c84b38bcb92048cdf51349a83faeaf
Ashish <thankiashish555@gmail.com>
2024-01-13 22:35:03 +0530

    Gallery Page Completed

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,69 +1,89 @@
+const importAll = (importMeta) => Object.values(importMeta);
+const bandhanImages = importAll(
+  import.meta.glob("../assets/images/Bandhan/*.{jpg,jpeg,png,svg}")
+);
+
+const jobFairImages = importAll(
+  import.meta.glob("../assets/images/JobFair/*.{jpg,jpeg,png,svg}")
+);
+
+const otherImages = importAll(
+  import.meta.glob("../assets/images/OtherEvents/*.{jpg,jpeg,png,svg}")
+);
+
+import { useState, useEffect } from "react";
+
 const Gallery = () => {
+  const [bandhan, setBandhan] = useState([]);
+  const [jobfair, setJobFair] = useState([]);
+  const [other, setOther] = useState([]);
+
+  useEffect(() => {
+    Promise.all(bandhanImages.map((imagePromise) => imagePromise()))
+      .then((images) => setBandhan(images))
+      .catch((error) => console.error(error));
+
+    Promise.all(jobFairImages.map((imagePromise) => imagePromise()))
+      .then((images) => setJobFair(images))
+      .catch((error) => console.error(error));
+
+    Promise.all(otherImages.map((imagePromise) => imagePromise()))
+      .then((images) => setOther(images))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="container">
-      <h1>Gallery</h1>
-      <section className="events">
-        <ul className="results">
-          <li className="result event">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=500&amp;ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTY2MjEyMDE3MQ&amp;ixlib=rb-1.2.1&amp;q=80&amp;utm_campaign=api-credit&amp;utm_medium=referral&amp;utm_source=unsplash_source&amp;w=500"
-                width="500"
-                height="500"
-                alt=""
-              />
-            </a>
-          </li>
-          <li className="result event">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=500&amp;ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTY2MjEyMDE3MQ&amp;ixlib=rb-1.2.1&amp;q=80&amp;utm_campaign=api-credit&amp;utm_medium=referral&amp;utm_source=unsplash_source&amp;w=500"
-                width="500"
-                height="500"
-                alt=""
-              />
-            </a>
-          </li>
-          <li className="result event">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=500&amp;ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTY2MjEyMDE3MQ&amp;ixlib=rb-1.2.1&amp;q=80&amp;utm_campaign=api-credit&amp;utm_medium=referral&amp;utm_source=unsplash_source&amp;w=500"
-                width="500"
-                height="500"
-                alt=""
-              />
-            </a>
-          </li>
-          <li className="result event">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=500&amp;ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTY2MjEyMDE3MQ&amp;ixlib=rb-1.2.1&amp;q=80&amp;utm_campaign=api-credit&amp;utm_medium=referral&amp;utm_source=unsplash_source&amp;w=500"
-                width="500"
-                height="500"
-                alt=""
-              />
-            </a>
-          </li>
-          <li className="result event">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=500&amp;ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTY2MjEyMDE3MQ&amp;ixlib=rb-1.2.1&amp;q=80&amp;utm_campaign=api-credit&amp;utm_medium=referral&amp;utm_source=unsplash_source&amp;w=500"
-                width="500"
-                height="500"
-                alt=""
-              />
-            </a>
-          </li>
-          <li className="result event">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=500&amp;ixid=MnwxfDB8MXxyYW5kb218MHx8bGFuZHNjYXBlfHx8fHx8MTY2MjEyMDE3MQ&amp;ixlib=rb-1.2.1&amp;q=80&amp;utm_campaign=api-credit&amp;utm_medium=referral&amp;utm_source=unsplash_source&amp;w=500"
-                width="500"
-                height="500"
-                alt=""
-              />
-            </a>
-          </li>
+      <section className="events bandhan">
+        <h1>
+          <span className="highlight-text">BANDHAN</span> - PARICHAY MELA
+        </h1>
+        <ul>
+          {bandhan.map((image, index) => (
+            <li key={index}>
+              <a href="">
+                <figure>
+                  <img src={image.default} alt={`Bandhan - ${index + 1}`} />
+                  <figcaption>Bandhan</figcaption>
+                </figure>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="events jobfair">
+        <h1>JOB FAIR</h1>
+        <ul>
+          {jobfair.map((image, index) => (
+            <li key={index}>
+              <a href="">
+                <figure>
+                  <img src={image.default} alt={`Jobfair - ${index + 1}`} />
+                  <figcaption>JobFair</figcaption>
+                </figure>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="events other-events">
+        <h1>OTHER EVENTS</h1>
+        <ul>
+          {other.map((image, index) => (
+            <li key={index}>
+              <a href="">
+                <figure>
+                  <img
+                    src={image.default}
+                    alt={`Other Events - ${index + 1}`}
+                  />
+                  <figcaption>Other</figcaption>
+                </figure>
+              </a>
+            </li>
+          ))}
         </ul>
       </section>
     </div>
