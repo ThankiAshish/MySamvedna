94da4cf3f4b133131a0fe2cbe0a180be932d3103
Ashish <thankiashish555@gmail.com>
2024-01-27 15:18:36 +0530

    Fix

--- api/controllers/postJob.php
+++ api/controllers/postJob.php
@@ -2,128 +2,124 @@
 include '../includes/config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $stmt = $conn->prepare("SELECT * FROM job_seeker where email = ?");
-    $stmt->bind_param("s", $_POST['email']);
+    $stmt = $conn->prepare("SELECT * FROM recruiters where recruiters_id = ?");
+    $stmt->bind_param("s", $_POST['recruiter_id']);
     $stmt->execute();
-
+    
     $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        $message = 'Email already exists';
+    
+    if ($result->num_rows < 1) {
+        $message = 'Recruiter does not exist';
         $response = array(
             'success' => false,
             'message' => $message,
         );
-
+        
         header('Content-Type: application/json');
         $jsonResponse = json_encode($response);
         echo $jsonResponse;
         exit();
     }
-
-    $companyName = $data['companyName'];
-    $website = $data['website'];
-    $natureOfBusiness = $data['natureOfBusiness'];
-    $address = $data['address'];
-    $fax = $data['fax'];
-    $areaCode = $data['areaCode'];
-    $landline = $data['landline'];
-    $mobile = $data['mobile'];
-    $email = $data['email'];
-    $employerName = $data['employerName'];
-    $companyDescription = $data['companyDescription'];
-    $jobDesignation = $data['jobDesignation'];
-    $jobType = $data['jobType'];
-    $dutyDescription = $data['dutyDescription'];
-    $essentialQualificationEssential = $data['essentialQualification']['essential'];
-    $essentialQualificationDesirable = $data['essentialQualification']['desirable'];
-    $ageLimit = $data['ageLimit'];
-    $womenEligible = $data['womenEligible'];
-    $workingHoursFrom = $data['workingHours']['from'];
-    $workingHoursTo = $data['workingHours']['to'];
-    $vacanciesRegular = $data['vacancies']['regular'];
-    $vacanciesTemporary = $data['vacancies']['temporary'];
-    $payAndAllowances = $data['payAndAllowances'];
-    $placeOfWork = $data['placeOfWork'];
-    $resumesToBeSent = $data['resumesToBeSent'];
-    $resumeEmail = $data['resumeEmail'];
-    $interviewDetailsDate = $data['interviewDetails']['date'];
-    $interviewDetailsTime = $data['interviewDetails']['time'];
-    $interviewDetailsAptitudeTest = $data['interviewDetails']['aptitudeTest'];
-    $interviewDetailsTechnicalTest = $data['interviewDetails']['technicalTest'];
-    $interviewDetailsGroupDiscussion = $data['interviewDetails']['groupDiscussion'];
-    $interviewDetailsPersonalInterview = $data['interviewDetails']['personalInterview'];
-    $interviewDetailsTopics = $data['interviewDetails']['topics'];
-    $interviewDetailsContactPerson = $data['interviewDetails']['contactPerson'];
-    $disabilityInfoType = $data['disabilityInfo']['type'];
-    $disabilityInfoPercentage = $data['disabilityInfo']['percentage'];
-    $disabilityInfoAidOrAppliance = $data['disabilityInfo']['aidOrAppliance'];
-    $ownVehiclePreferred = $data['ownVehiclePreferred'];
-    $conveyanceProvided = $data['conveyanceProvided'];
-    $conveyanceType = $data['conveyanceType'];
-    $otherInformation = $data['otherInformation'];
     
+    $query = "INSERT INTO job (
+        `recruiters_id`, `companyName`, `website`, `natureOfBusiness`, 
+        `address`, `fax`, `areaCode`, `landline`, `mobile`, 
+        `email`, `employerName`, `companyDescription`, `jobDesignation`, `jobType`, 
+        `dutyDescription`, `essentialQualificationEssential`, `essentialQualificationDesirable`, `ageLimit`, `womenEligible`, 
+        `workingHoursFrom`, `workingHoursTo`, `vacanciesRegular`, `vacanciesTemporary`, `payAndAllowances`, 
+        `placeOfWork`, `resumesToBeSent`, `resumeEmail`, `interviewDetailsDate`, `interviewDetailsTime`, 
+        `interviewDetailsAptitudeTest`, `interviewDetailsTechnicalTest`, `interviewDetailsGroupDiscussion`, `interviewDetailsPersonalInterview`, `interviewDetailsTopics`, 
+        `interviewDetailsContactPerson`, `disabilityInfoType`, `disabilityInfoPercentage`, `disabilityInfoAidOrAppliance`, `ownVehiclePreferred`, 
+        `conveyanceProvided`, `conveyanceType`, `otherInformation`) 
+        VALUES (
+            ?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?,?,?,
+            ?,?,?)";
 
-    $sql = "INSERT INTO `job_seeker`(`email`, `username`, `password`, `name`,`lastName`,
-    `dob`, `gender`, `permanentAddress`, `currentAddress`,`city`,
-     `state`, `postalCode`, `country`, `contactNumber`,`whatsappNumber`,
-      `jobAlerts`, `homePhone`, `addHomePhone`, `qualification`,`educationSpecialization`,
-       `experienceAndAppliance`, `yesNoQuestion`, `twoWheeler`, `threeWheeler`, `car`, `specializationInDisability`) VALUES
-        (?,?,?,?,?,?
-        ,?,?,?,?,?,?
-        ,?,?,?,?,?,?,
-        ?,?,?,?,?,?,
-        ?,?)";
-   
-   $stmt = $conn->prepare($sql);
-   
-   // Bind the data to the SQL statement
-   $stmt->bind_param(
-       "ssssssssssssssssssssssssss",
-       $email,
-       $username,
-       $password,
-       $name,
-       $lastName,
-       $dob,
-       $gender,
-       $permanentAddress,
-       $currentAddress,
-       $city,
-       $state,
-       $postalCode,
-       $country,
-       $contactNumber,
-       $whatsappNumber,
-       $jobAlerts,
-       $homePhone,
-       $addHomePhone,
-       $qualification,
-       $educationSpecialization,
-       $experienceAndAppliance,
-       $yesNoQuestion,
-       $twoWheeler,
-       $threeWheeler,
-       $car,
-       $specializationInDisability
-   );
-   
+    print_r($stmt = $conn->prepare($query));
+    die();
 
-    if (!$stmt->execute()) {
-        die('Error in execute statement: ' . $stmt->error);
-    } else {
-        $message = 'Registration Successful!';
-        $response = array(
-            'success' => true,
-            'message' => $message,
-        );
+    $stmt->bind_param(
+        "ssss
+         sssss
+         sssss
+         sssss
+         sssss
+         sssss
+         sssss
+         sssss
+         sss",
+        $_POST['recruiters_id'],
+        $_POST['companyName'],
+        $_POST['website'],
+        $_POST['natureOfBusiness'],
+        $_POST['address'],
+        $_POST['fax'],
+        $_POST['areaCode'],
+        $_POST['landline'],
+        $_POST['mobile'],
+        $_POST['email'],
+        $_POST['employerName'],
+        $_POST['companyDescription'],
+        $_POST['jobDesignation'],
+        $_POST['jobType'],
+        $_POST['dutyDescription'],
+        $_POST['essentialQualificationEssential'],
+        $_POST['essentialQualificationDesirable'],
+        $_POST['ageLimit'],
+        $_POST['womenEligible'],
+        $_POST['workingHoursFrom'],
+        $_POST['workingHoursTo'],
+        $_POST['vacanciesRegular'],
+        $_POST['vacanciesTemporary'],
+        $_POST['payAndAllowances'],
+        $_POST['placeOfWork'],
+        $_POST['resumesToBeSent'],
+        $_POST['resumeEmail'],
+        $_POST['interviewDetailsDate'],
+        $_POST['interviewDetailsTime'],
+        $_POST['interviewDetailsAptitudeTest'],
+        $_POST['interviewDetailsTechnicalTest'],
+        $_POST['interviewDetailsGroupDiscussion'],
+        $_POST['interviewDetailsPersonalInterview'],
+        $_POST['interviewDetailsTopics'],
+        $_POST['interviewDetailsContactPerson'],
+        $_POST['disabilityInfoType'],
+        $_POST['disabilityInfoPercentage'],
+        $_POST['disabilityInfoAidOrAppliance'],
+        $_POST['ownVehiclePreferred'],
+        $_POST['conveyanceProvided'],
+        $_POST['conveyanceType'],
+        $_POST['otherInformation']
+    );
 
-        header('Content-Type: application/json');
-        $jsonResponse = json_encode($response);
-        echo $jsonResponse;
-    }
+    $stmt->execute();
 
-    $stmt->close();
+    $message = 'Job posted successfully';
+    $response = array(
+        'success' => true,
+        'message' => $message,
+    );
 
-    $conn->close();
+    header('Content-Type: application/json');
+    $jsonResponse = json_encode($response);
+    echo $jsonResponse;
+} else {
+    $message = 'Invalid request method';
+    $response = array(
+        'success' => false,
+        'message' => $message,
+    );
+
+    header('Content-Type: application/json');
+    $jsonResponse = json_encode($response);
+    echo $jsonResponse;
+
+    exit();
 }
